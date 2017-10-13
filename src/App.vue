ea83c272e7ddd79af13787f0712a5f25c8594c0e
kirin-yuen <ywl_allen@163.com>
2017-10-13 12:29:59 +0800

    typical vuejs appcalition without vuex

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <product-list-one :products="products"></product-list-one>
+        <product-list-two :products="products"></product-list-two>
+    </div>
+</template>
+<script>
+import ProductListOne from './components/product-list-one.vue';
+import ProductListTwo from './components/product-list-two.vue';
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            // store share the data on the root component and pass it down which need
+            products: [
+                { name: 'Banana Skin', price: 20 },
+                { name: 'Shiny Star', price: 40 },
+                { name: 'Green Shells', price: 60 },
+                { name: 'Red Shells', price: 80 }
+            ]
+        }
+    },
+    components: {
+        'product-list-one': ProductListOne,
+        'product-list-two': ProductListTwo
+    }
+}
+</script>
+<style lang="scss">
+</style>
